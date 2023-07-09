9cd6b40df882d4bec48f210f352bdd7ce63e906c
urcoderpal <adityapal971@gmail.com>
2023-07-09 09:49:46 +0530

    second commit

--- docker/src/main/java/com/example/demo/Controller.java
+++ docker/src/main/java/com/example/demo/Controller.java
@@ -12,8 +12,8 @@ public class Controller {
 	@RequestMapping("/")
 	public Map<String, Object> getValues() {
 		Map<String, Object> data = new HashMap<String, Object>();
-		data.put("message", "Java api is working fine");
-		data.put("languages", Arrays.asList("Java", "C#", "JavaScript"));
+		data.put("message", "My first Docker Trial");
+		data.put("friends", Arrays.asList("Aditya", "Kaustubh", "Ganesh"));
 		data.put("code", 2345);
 		return data;
 	}
